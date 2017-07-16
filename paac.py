be14289d5676ac54703278b51e8a16f8cf050238
SunYeop Lee <sunyeop97@gmail.com>
2017-07-17 01:57:18 +0900

    Add LSTM layer, --no-cudnn
    Add --no-cudnn for lesser use of memory

--- paac.py
+++ paac.py
@@ -1,10 +1,11 @@
+import torch
 import torch.nn as nn
+import torch.nn.init as init
 from torch.autograd import Variable
 from PIL import Image
 from PIL.Image import BILINEAR
 from torchvision.transforms import ToTensor, ToPILImage
 
-INPUT_CHANNELS = 4
 INPUT_IMAGE_SIZE = (84, 84)
 
 
@@ -16,7 +17,7 @@ class PAACNet(nn.Module):
         super().__init__()
 
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(INPUT_CHANNELS, 32, 8, 4),
+            nn.Conv2d(1, 32, 8, 4),
             nn.ReLU(),
             nn.Conv2d(32, 64, 4, 2),
             nn.ReLU(),
@@ -24,7 +25,8 @@ class PAACNet(nn.Module):
             nn.ReLU()
         )
 
-        self.fc = nn.Linear(3136, 512)
+        self.lstm = nn.LSTM(3136, 256)
+        self.fc = nn.Linear(256, 512)
 
         self.policy_output = nn.Sequential(
             nn.Linear(512, num_actions),
@@ -34,8 +36,6 @@ class PAACNet(nn.Module):
         self.value_output = nn.Linear(512, 1)
 
         # init weights and biases
-        import torch.nn.init as init
-
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 init.kaiming_normal(m.weight)
@@ -43,6 +43,12 @@ class PAACNet(nn.Module):
             elif isinstance(m, nn.Linear):
                 init.kaiming_normal(m.weight)
                 m.bias.data.zero_()
+            elif isinstance(m, nn.LSTM):
+                for name, param in m.named_parameters():
+                    if 'weight' in name:
+                        init.kaiming_normal(param)
+                    elif 'bias' in name:
+                        param.data.zero_()
 
     @classmethod
     def preprocess(cls, x):
@@ -55,31 +61,33 @@ class PAACNet(nn.Module):
         # TODO : support flickering games by picking max pixels
         x = Image.fromarray(x.squeeze(), 'L')
         x = x.resize(INPUT_IMAGE_SIZE, resample=BILINEAR)
-        return cls.to_tensor(x)
+        return cls.to_tensor(x) * 2. - 1.
 
-    def forward(self, x):
+    def forward(self, x, hidden=None):
         r"""calculates PAAC outputs
 
         :param x: preprocessed states with shape (N, H, W, C)
         :return: tuple (policy_output, value_output)
         """
         x = self.conv_layers(x)
-        # flatten
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return self.policy_output(x), self.value_output(x)
+        x, hidden = self.lstm(x.unsqueeze(0), hidden)
+        x = self.fc(x.squeeze(0))
+        return self.policy_output(x), self.value_output(x), hidden
 
-    def policy(self, x):
+    def policy(self, x, hidden=None):
         x = self.conv_layers(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return self.policy_output(x)
+        x, hidden = self.lstm(x.unsqueeze(0), hidden)
+        x = self.fc(x.squeeze(0))
+        return self.policy_output(x), hidden
 
-    def value(self, x):
+    def value(self, x, hidden=None):
         x = self.conv_layers(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return self.value_output(x)
+        x, hidden = self.lstm(x.unsqueeze(0), hidden)
+        x = self.fc(x.squeeze(0))
+        return self.value_output(x), hidden
 
     @staticmethod
     def entropy(x, epsilon=0):
@@ -114,3 +122,6 @@ class PAACNet(nn.Module):
         double_loss_v = diff.pow(2).mean()
         loss = loss_p + 0.25 * double_loss_v
         return loss_p, double_loss_v, loss
+
+    def get_initial_hidden(self, batch_size):
+        return torch.zeros(1, batch_size, self.lstm.hidden_size)
